4dcfae8b0998fa03a4bd5aa6f73f3382b24b96e0
JoudHijaz <joudhijaz2021@gmail.com>
2024-12-11 22:14:40 +0200

    Refactor header assets and update styles
    - Removed unused header menu-list images
    - Added new image to header right-container
    - Deleted obsolete images from header right-container
    - Updated styles in css/index.css
    - Modified gulpfile.js for build process adjustments
    - Updated index.html structure
    - Refined SCSS variables and header partials

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,11 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
-
 gulp.task('sass', function () {
-    return gulp.src('./scss/index.scss') // Make sure this path points to your index.scss
-        .pipe(sass().on('error', sass.logError))
-        .pipe(gulp.dest('./css')); // Adjust this path as needed for where you want your compiled CSS to go
-});
-
+    return gulp.src('./scss/index.scss')
+      .pipe(sass().on('error', sass.logError))
+      .pipe(gulp.dest('./css'));
+  });
+  
 gulp.task('watch', function () {
     gulp.watch('./scss/**/*.scss', gulp.series('sass'));
 });
